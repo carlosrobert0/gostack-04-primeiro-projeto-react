aeb5816806700b78477d15e7a9098fa59decdc80
Carlos Roberto <carlosroberto15755@gmail.com>
2020-07-18 16:04:09 -0300

    Estelizada interface Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,61 @@
 import React from 'react'
+import { FiChevronRight } from 'react-icons/fi'
 
-import { Title } from './styles'
+import logoImg from '../../assets/logo.svg'
+
+import { Title, Form, Repositories } from './styles'
 
 const Dashboard: React.FC = () => {
-  return <Title>Explorer repositórios no Github</Title>
+  return (
+    <>
+      <img src={logoImg} alt='Github Explorer'/>
+      <Title>Explore repositórios no Github.</Title>
+
+      <Form>
+        <input placeholder='Digite o nome do repositório'/>
+        <button type='submit'>Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href='test'>
+          <img
+            src='https://avatars2.githubusercontent.com/u/45858897?s=460&u=8dfeb7430dc4d8ac2b6cff7e0975a81dcd3dd397&v=4'
+            alt='Carlos Roberto'
+          />
+          <div>
+            <strong>carloscamacho</strong>
+            <p>multinacional conect</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href='test'>
+          <img
+            src='https://avatars2.githubusercontent.com/u/45858897?s=460&u=8dfeb7430dc4d8ac2b6cff7e0975a81dcd3dd397&v=4'
+            alt='Carlos Roberto'
+          />
+          <div>
+            <strong>carloscamacho</strong>
+            <p>multinacional conect</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href='test'>
+          <img
+            src='https://avatars2.githubusercontent.com/u/45858897?s=460&u=8dfeb7430dc4d8ac2b6cff7e0975a81dcd3dd397&v=4'
+            alt='Carlos Roberto'
+          />
+          <div>
+            <strong>carloscamacho</strong>
+            <p>multinacional conect</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  )
 }
 
-export default Dashboard
+export default Dashboard;
